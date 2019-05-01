89d7458a5d01313a5e49bba9104dbf6a34bb74b9
Kadia Toure <kadiditoure@gmail.com>
2019-05-02 01:37:19 +0200

    feature: webpack-encore

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,25 +1,29 @@
-<?php 
+<?php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\ContactType;
+use App\Form\PropertySearchType;
+use App\Notification\ContactNotification;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\PropertySearch;
-use App\Form\PropertySearchType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class PropertyController extends  AbstractController
+class PropertyController extends AbstractController
 {
+    
     /**
      * @var PropertyRepository
-     * */
+     */
     private $repository;
     /**
-     * 
      * @var ObjectManager
      */
     private $em;
@@ -29,47 +33,62 @@ class PropertyController extends  AbstractController
         $this->repository = $repository;
         $this->em = $em;
     }
+    
     /**
      * @Route("/biens", name="property.index")
      * @return Response
-     * */
-    public function index(PaginatorInterface $paginator, Request $request):Response
-    {   
-        //form research
+     */
+    public function index(PaginatorInterface $paginator, Request $request): Response
+    {
         $search = new PropertySearch();
         $form = $this->createForm(PropertySearchType::class, $search);
         $form->handleRequest($request);
         
-        //pagination
-        $properties = $paginator->paginate($this->repository->findAllVisibleQuery($search),
-        $request->query->getInt('page', 1), /*page number*/
-        15 /*limit per page*/
-        );
-        
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery($search),
+            $request->query->getInt('page', 1),
+            12
+            );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
-            'properties' => $properties,
-            'form' => $form->createView()
+            'properties'   => $properties,
+            'form'         => $form->createView()
         ]);
     }
     
     /**
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
-     * @param Property property
+     * @param Property $property
      * @return Response
-     * */
-    public function show(Property $property, string $slug): Response
+     */
+    public function show(Property $property, string $slug, Request $request, ContactNotification $notification): Response
     {
-        if ($property->getSlug() !== $slug)
-        {
+        if ($property->getSlug() !== $slug) {
             return $this->redirectToRoute('property.show', [
-                'id' => $property->getId(),
+                'id'   => $property->getId(),
                 'slug' => $property->getSlug()
             ], 301);
         }
+        
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre email a bien été envoyé');
+            return $this->redirectToRoute('property.show', [
+                'id'   => $property->getId(),
+                'slug' => $property->getSlug()
+            ]);
+        }
+        
         return $this->render('property/show.html.twig', [
-            'property' => $property,
-            'current_menu' => 'properties'
+            'property'     => $property,
+            'current_menu' => 'properties',
+            'form'         => $form->createView()
         ]);
     }
+    
 }
